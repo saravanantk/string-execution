5144a342ca6260af7b7dc5c99f7fdf4af09234c0
saravanantk <sarvanmech28@gmail.com>
2022-06-18 15:26:19 +0530

    string execution

---
+++ src/com/catti/CattyDj.java
@@ -0,0 +1,60 @@
+package com.catti;
+
+import java.util.List;
+import java.util.Vector;
+
+public class CattyDj {
+
+
+		public static void main(String[] args) {
+			
+
+			
+			List v = new Vector<>();
+			
+			v.add(20);
+			v.add(40);
+			v.add(50);
+			v.add(60);
+			v.add(80);
+			v.add(90);
+			
+			System.out.println(v);
+			
+			int size = v.size();
+			System.out.println(size);
+			
+			Object get = v.get(5);
+			System.out.println(get);
+			
+		    v.add(2, 10);
+		    System.out.println(v);
+		    
+		    v.set(5, 99);
+		    System.out.println(v);
+		    
+		    v.remove(0);
+		    System.out.println(v);
+		    
+		    boolean contains = v.contains(99);
+		    System.out.println(contains);
+		    
+		    int indexof = v.indexOf(90);
+		    System.out.println(indexof);
+		    
+		    int lastindexof = v.lastIndexOf(99);
+		    System.out.println(lastindexof);
+		    
+		    v.clear();
+		    System.out.println(v);
+		    
+		    boolean empty = v.isEmpty();
+		    System.out.println(empty);
+			
+	
+	
+		}	
+	
+}
+	
+
